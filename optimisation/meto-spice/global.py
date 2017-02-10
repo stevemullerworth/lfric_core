8916b8d5a4d655abc75e3cc0c71b34f9f2946efd
matthewhambley <enquiries@metoffice.gov.uk>
2017-02-10 10:25:20 +0000

    ticket:836 for Steve Mullerworth: Adopt PSyclone 1.3.1
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@9022 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- optimisation/meto-spice/global.py
+++ optimisation/meto-spice/global.py
@@ -5,11 +5,12 @@
 # https://puma.nerc.ac.uk/trac/GungHo/wiki
 ##############################################################################
 
-from transformations import Dynamo0p3ColourTrans, DynamoOMPParallelLoopTrans
+from transformations import Dynamo0p3ColourTrans, Dynamo0p3OMPLoopTrans,OMPParallelTrans
 
 def trans(psy):
     ctrans = Dynamo0p3ColourTrans()
-    otrans = DynamoOMPParallelLoopTrans()
+    otrans = Dynamo0p3OMPLoopTrans()
+    oregtrans = OMPParallelTrans()
 
     # Loop over all of the Invokes in the PSy object
     for invoke in psy.invokes.invoke_list:
@@ -17,15 +18,16 @@ def trans(psy):
         print "Transforming invoke '{0}' ...".format(invoke.name)
         schedule = invoke.schedule
 
-        # Colour loops unless they are on W3
+        # Colour loops unless they are on W3 or over dofs
         for loop in schedule.loops():
-            if loop.field_space != "w3":
-                schedule, _ = ctrans.apply(loop)
+            if loop.iteration_space == "cells" and loop.field_space != "w3":
+                 schedule, _ = ctrans.apply(loop)
 
         # Add OpenMP to loops unless they are over colours
         for loop in schedule.loops():
             if loop.loop_type != "colours":
-                schedule, _ = otrans.apply(loop)
+                schedule, _ = oregtrans.apply(loop)
+                schedule, _ = otrans.apply(loop, reprod=True)
 
         # take a look at what we've done
         schedule.view()
